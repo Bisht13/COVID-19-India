1b7474d44845faf1f03b755241bcbeee6b44077e
Aditya Bisht <adityabisht64@gmail.com>
2020-08-06 14:26:24 +0530

    Basic Collapsible Toolbar

--- app/src/main/java/com/adityabisht/covid_19india/MainActivity.java
+++ app/src/main/java/com/adityabisht/covid_19india/MainActivity.java
@@ -3,6 +3,9 @@ package com.adityabisht.covid_19india;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
     }
+
 }
