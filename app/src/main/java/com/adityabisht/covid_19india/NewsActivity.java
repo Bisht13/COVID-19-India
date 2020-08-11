85b695ae94a54e94b17a37cd2c32be97ae96f0f4
Aditya Bisht <adityabisht64@gmail.com>
2020-08-12 02:13:53 +0530

    Working on News Tab #1

--- app/src/main/java/com/adityabisht/covid_19india/NewsActivity.java
+++ app/src/main/java/com/adityabisht/covid_19india/NewsActivity.java
@@ -1,20 +1,48 @@
 package com.adityabisht.covid_19india;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.adityabisht.covid_19india.api.ApiInterface;
+import com.adityabisht.covid_19india.api.apiClient;
+import com.adityabisht.covid_19india.models.Article;
+import com.adityabisht.covid_19india.models.News;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class NewsActivity extends AppCompatActivity {
     private Button backbutton;
+    public static final String API_KEY = "1adf075e75644c16835b104240bee180";
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private List<Article> articles = new ArrayList<>();
+    private NewsAdapter newsAdapter;
+    private String TAG = NewsActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
 
+        recyclerView = findViewById(R.id.recyclerView);
+        layoutManager = new LinearLayoutManager(NewsActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setNestedScrollingEnabled(false);
         backbutton = findViewById(R.id.backbutton);
         backbutton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,9 +50,43 @@ public class NewsActivity extends AppCompatActivity {
                 backbutton();
             }
         });
+        LoadJson();
     }
     public void backbutton(){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+    public void LoadJson(){
+        ApiInterface apiInterface = apiClient.getApiClient().create(ApiInterface.class);
+
+        String country = Utils.getCountry();
+
+        Call<News> call;
+        call = apiInterface.getNews(country, API_KEY);
+
+        call.enqueue(new Callback<News>() {
+            @Override
+            public void onResponse(Call<News> call, Response<News> response) {
+                if (response.isSuccessful() && response.body().getArticle() != null){
+
+                    if (!articles.isEmpty()){
+                        articles.clear();
+                    }
+
+                    articles = response.body().getArticle();
+                    newsAdapter = new NewsAdapter(articles, NewsActivity.this);
+                    recyclerView.setAdapter(newsAdapter);
+                    newsAdapter.notifyDataSetChanged();
+
+                } else {
+                    Toast.makeText(NewsActivity.this, "No Result!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<News> call, Throwable t) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/adityabisht/covid_19india/api/ApiInterface.java
@@ -0,0 +1,17 @@
+package com.adityabisht.covid_19india.api;
+
+import com.adityabisht.covid_19india.models.News;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiInterface {
+
+    @GET("top-headlines")
+    Call<News> getNews(
+            @Query("country") String country,
+            @Query("apiKey") String apiKey
+    );
+
+}
