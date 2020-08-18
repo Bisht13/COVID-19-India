eda21d9fa9487e8d4f1f1faba0156fd02bd0b68d
Aditya Bisht <adityabisht64@gmail.com>
2020-08-18 21:56:06 +0530

    Precautions Tab

--- app/src/main/java/com/adityabisht/covid_19india/PrecautionsActivity.java
+++ app/src/main/java/com/adityabisht/covid_19india/PrecautionsActivity.java
@@ -3,12 +3,15 @@ package com.adityabisht.covid_19india;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class PrecautionsActivity extends AppCompatActivity {
     private Button backbutton;
+    private TextView link;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +19,20 @@ public class PrecautionsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_precautions);
 
         backbutton = findViewById(R.id.backbutton);
+        link = findViewById(R.id.link);
         backbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 backbutton();
+            }
+        });
+
+        link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uriUrl = Uri.parse("https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public");
+                Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+                startActivity(launchBrowser);
             }
         });
     }
